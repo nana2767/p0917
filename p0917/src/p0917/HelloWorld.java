0fcafc997b24fcf81dd436cd8c5a771012b2fce5
13 <13@13-PC>
2018-09-17 10:17:28 +0900

    p0917 update message

--- p0917/src/p0917/HelloWorld.java
+++ p0917/src/p0917/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!!!");
+		System.out.println("헬로우월드!!!");
+		System.out.println("헬로우월드22!!!");
 	}
 
 }
